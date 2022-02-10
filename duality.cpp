fb3a340e4ef18f23416298623930a75350c95b57
Aeluxx <matthewgregory@me.com>
2022-02-10 12:07:22 -0500

    Add files via upload

---
+++ duality.cpp
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main() {
+	int pid = fork();
+	if (pid == 0){
+		int runs = 0;
+		while (true){
+			runs += 1;
+			std::cout << "Run #" << runs << "\n";
+			std::this_thread::sleep_for (std::chrono::milliseconds(1));
+		}
+	} else {
+		char test[3];
+		sprintf(test, "%d", pid);
+	
+		char *arguments[3];
+		arguments[0] = (char*)"./kill";
+		arguments[1] = strdup(test);
+		arguments[2] = NULL;
+
+		//Question code goes here
+	
+		std::cout << "finish them?\n";
+		execvp(arguments[0], arguments);
+	}
+}	
